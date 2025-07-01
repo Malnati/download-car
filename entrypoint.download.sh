2ab15813202cd3543127d97395827c5c22bd83f3
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2025-07-01 10:57:34 -0300

    Add Docker build files and entrypoints

---
+++ entrypoint.download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# entrypoint.download.sh
+set -e
+
+STATE=${STATE:-DF}
+POLYGON=${POLYGON:-APPS}
+FOLDER=${FOLDER:-data/DF}
+TRIES=${TRIES:-25}
+DEBUG=${DEBUG:-False}
+TIMEOUT=${TIMEOUT:-30}
+MAX_RETRIES=${MAX_RETRIES:-5}
+
+exec python examples/download_state.py \
+    --state "$STATE" \
+    --polygon "$POLYGON" \
+    --folder "$FOLDER" \
+    --tries "$TRIES" \
+    --debug "$DEBUG" \
+    --timeout "$TIMEOUT" \
+    --max_retries "$MAX_RETRIES"
